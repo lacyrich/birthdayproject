44d31c77d1378aa041fcc3a8e9226bb9f5b19904
Lacy <44073703+lacyrich@users.noreply.github.com>
2022-12-31 13:43:29 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace mis221_lab3_lacyrich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = GetDayOfWeek();
+            BirthdayDecision(day);
+        }
+        static string GetDayOfWeek()
+        {
+            Console.WriteLine("Enter the day of the week:");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static void BirthdayDecision(string day)
+        {
+            if(day == "Monday")
+            {
+                SayHappyBirthday("Joseph");
+            } 
+            else if (day == "Thursday")
+            {
+                SayHappyBirthday("Katie");
+            }
+            else{
+                Console.WriteLine("There are no birthdays today :(");
+            }
+        }
+
+        static void SayHappyBirthday(string name)
+        {Console.WriteLine("Happy Birthday, " + name + "!");
+        }
+    }
+}
